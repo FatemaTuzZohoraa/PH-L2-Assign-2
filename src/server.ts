b1640b65de0160f75f70129293b6c8adc572ac12
FatemaTuzZohoraa <fatema.tuz.zohora.070@gmail.com>
2026-06-07 12:24:07 +0600

    created tables and called func

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import app from "./app";
+import config from "./config";
+import { initDB } from "./db"
+
 
 const main=()=>{
-   
-    
+    initDB();
+    app.listen(config.port,()=>{
+      console.log(`APP LISTENING ON PORT ${config.port}`);
+    })
 }
 main()
