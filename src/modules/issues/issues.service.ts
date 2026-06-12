1120b66922fd544bdb9a20da0030a6b1b0a5385d
FatemaTuzZohoraa <fatema.tuz.zohora.070@gmail.com>
2026-06-12 22:49:13 +0600

    issues creation on db done

--- src/modules/issues/issues.service.ts
+++ src/modules/issues/issues.service.ts
@@ -1,7 +1,57 @@
-const createIssuesIntoDB=async(payload:any)=>{
+import { pool } from "../../db"
+import type { IIssue } from "./issues.interface";
 
+const createIssueIntoDB=async(payload:IIssue)=>{const {title,description,type}=payload;
+
+
+const result=await pool.query(`
+  INSERT INTO Issues(title,description,type) VALUES($1,$2,$3)
+  RETURNING *
+  `,[title,description,type])
+
+  return result;
 }
 
-export const issuesController={
-  createIssuesIntoDB,
+const getAllIssuesFromDB=async()=>{
+const result=await pool.query(`
+      SELECT * FROM Issues
+      `)
+
+      return result;
+}
+const getSingleIssueFromDB=async(id:string)=>{
+  const result = await pool.query(`
+    SELECT * FROM Issues WHERE id=$1
+    `,[id])
+  return result
+}
+
+const updateIssueFromDB=async(payload:IIssue,id:string)=>{
+  const {title,description,type}=payload
+  const result = await pool.query(`UPDATE Issues 
+    SET 
+    title=COALESCE($1,title),
+    description=COALESCE($2,description),
+    type=COALESCE($3,type),
+    WHERE id=$4 
+    RETURNING *
+    `,[title,description,type,id])
+    
+    return result;
+
+}
+const deleteIssueFromDB=async(id:string)=>{
+   const result = await pool.query(`
+    DELETE FROM Issues WHERE id=$1
+    
+    `,[id])
+    return result
+}
+
+export const IssueService={
+  createIssueIntoDB,
+  getAllIssuesFromDB,
+  getSingleIssueFromDB,
+  updateIssueFromDB,
+  deleteIssueFromDB
 }
